e0f998d3b41b80bbd5cfb66c6a085afac22c53ac
sawdustofmind <sawdustofmind@gmail.com>
2026-05-09 11:56:42 +0200

    chore: initial project setup

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+use std::net::TcpListener;
+use sqlx::PgPool;
+use newsletter::startup::run;
+use newsletter::configuration::get_configuration;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
+    run(listener, connection_pool)?.await
+}
