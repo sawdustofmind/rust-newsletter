a5c582a68062b0596668a1343bfe90e1a7ff16ef
sawdustofmind <sawdustofmind@gmail.com>
2026-05-12 12:41:56 +0200

    chore: configure deployment

--- src/main.rs
+++ src/main.rs
@@ -10,10 +10,10 @@ async fn main() -> std::io::Result<()> {
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+    let connection_pool = PgPool::connect_with(configuration.database.with_db())
         .await
         .expect("Failed to connect to Postgres.");
-    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let address = format!("{}:{}", configuration.application.host, configuration.application.port);
     let listener = TcpListener::bind(address)?;
     run(listener, connection_pool)?.await
 }
